733a2ea702f0226fbcf96d17bd208d422f8f82ce
Sandesh-PG <sandeshsandy783@gmail.com>
2026-04-05 08:01:57 +0530

    minor changes in websocket

--- src/ws/server.js
+++ src/ws/server.js
@@ -18,16 +18,31 @@ function broadcast(wss, payload) {
 }
 
 export function attachWebSocketServer(server) {
-    const wss = new WebSocketServer({ server, path: '/ws', maxPayload: 1024 * 1024});
+  const wss = new WebSocketServer({ server, path: '/ws', maxPayload: 1024 * 1024 });
 
-    wss.on('connection', (ws) => {
-        sendJson(ws, { type: 'welcome', message: 'Welcome to the Sportz WebSocket API' });
+  wss.on('connection', (socket) => {
+    socket.isAlive = true;
+    socket.on('pong', () => { socket.isAlive = true; });
+
+    sendJson(socket, { type: 'welcome' });
+
+    socket.on('error', console.error);
+  });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) return ws.terminate();
+
+      ws.isAlive = false;
+      ws.ping();
     });
+  }, 30000);
 
-    function broadcastMatchCreated(match) {
-        broadcast(wss, { type: 'match_created', data: match });
-    }
+  wss.on('close', () => clearInterval(interval));
 
-    return { broadcastMatchCreated };
+  function broadcastMatchCreated(match) {
+    broadcast(wss, { type: 'match_created', data: match });
+  }
 
-};
+  return { broadcastMatchCreated };
+}
